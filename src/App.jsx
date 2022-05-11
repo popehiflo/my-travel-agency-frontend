e0f6965de828a7bd7564667f0057e043b6af0385
popehiflo <poolpetterhijuel@hotmail.com>
2022-05-11 18:41:19 -0500

    feat: Header component created

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,7 @@
-import logo from './logo.svg';
+import Header from './components/Header';
 
 const App = () => (
-  <section className="App">
-    <header className="App-header">
-      <img src={logo} className="App-logo" alt="logo" />
-      <p>
-        Edit
-        {' '}
-        <code>src/App.js</code>
-        {' '}
-        and save to reload.
-      </p>
-      <a
-        className="btn-primary"
-        href="https://reactjs.org"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        Learn React hahahaha
-      </a>
-    </header>
-  </section>
+  <Header />
 );
 
 export default App;
