1a54767dbf7f069bdf144361e9886c6bbcf2f284
popehiflo <poolpetterhijuel@hotmail.com>
2022-05-14 12:04:01 -0500

    feat: TourCard done

--- src/components/TourCard.jsx
+++ src/components/TourCard.jsx
@@ -2,14 +2,13 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faCartPlus, faHeart, faMoon, faSun } from '@fortawesome/free-solid-svg-icons';
-import { faMoneyBill1 } from '@fortawesome/free-regular-svg-icons';
+import { faCartPlus, faDollarSign, faGripLinesVertical, faHeart, faMoon, faSun } from '@fortawesome/free-solid-svg-icons';
 import styledComponents from 'styled-components';
 
 const TourCardWrapper = styledComponents.div`
   background: var(--color-bg-white);
-  /* box-shadow: var(--primary-box-shadow); */
-  border: var(--border-primary-light);
+  box-shadow: var(--primary-box-shadow);
+  // border: var(--border-primary-light);
   border-radius: var(--border-radius);
   overflow: hidden;
   display: flex;
@@ -18,15 +17,14 @@ const TourCardWrapper = styledComponents.div`
 `;
 
 const TourCardInfo = styledComponents.div`
-  background: var(--color-bg-light); // quitar esto luego
   border-radius: var(--border-radius);
 `;
 
 const CardInfoImage = styledComponents.div`
   position: relative;
   height: 30rem;
-  border-radius: var(--border-radius);
-  box-shadow: var(--primary-box-shadow);
+  border-radius: var(--border-radius) var(--border-radius) 0 0;
+  // box-shadow: var(--primary-box-shadow);
   overflow: hidden;
   width: 100%;
   img {
@@ -48,19 +46,45 @@ const CardInfoContent = styledComponents.div`
     font-size: 1.8rem;
     color: var(--color-secondary);
     text-transform: uppercase;
+    margin-bottom: 0.5rem;
+  }
+  p {
+    font-size: 1.5rem;
+    color: var(--color-secondary);
+    margin-bottom: 0.5rem;
+  }
+`;
+
+const ContentIcons = styledComponents.div`
+  background: var(--color-bg-light);
+  width: max-content;
+  border-radius: var(--border-radius-btn-primary);
+  padding: 0.6rem 1.5rem;
+  span {
+    font-size: 1.7rem;
+    color: var(--color-secondary);
+    svg {
+      color: var(--color-primary);
+    }
   }
 `;
 
 const TourCardButtons = styledComponents.div`
-  background: skyblue;
-  padding: 0 0.5rem;
+  background: var(--color-primary);
+  padding: 1rem 0.5rem;
   display: flex;
-  justify-content: center;
+  flex-direction: column;
   align-items: center;
+  justify-content: center;
 `;
 
+const borderWhite = {
+  border: 'var(--border-primary-white)',
+};
+
 const TourCard = ({ tour }) => {
-  const { id, slug } = tour;
+  const { id, slug, information } = tour;
+  const litleInformation = information.slice(0, 100);
   return (
     <TourCardWrapper className="box-tour">
       <TourCardInfo className="box__tour">
@@ -76,28 +100,30 @@ const TourCard = ({ tour }) => {
           </div>
         </CardInfoImage>
         <CardInfoContent className="content">
-          {/* FIXME: mostrar solo 90 caracteres de description en p,con JS */}
           <h3>{tour.title}</h3>
-          <div className="icons">
+          {/* FIXME: mostrar solo 90 caracteres de description en p,con JS */}
+          <p>{`${litleInformation} ...`}</p>
+          <ContentIcons className="icons">
             <span className="day">
               <FontAwesomeIcon icon={faSun} />
-              {` ${tour.duration.days} días`}
+              {` ${tour.duration.days} dias`}
             </span>
+            <FontAwesomeIcon icon={faGripLinesVertical} style={{ fontSize: '1.5rem', margin: '0 0.6rem' }} />
             <span className="night">
               <FontAwesomeIcon icon={faMoon} />
               {` ${tour.duration.nights} noches`}
             </span>
+            <FontAwesomeIcon icon={faGripLinesVertical} style={{ fontSize: '1.5rem', margin: '0 0.6rem' }} />
             <span className="money">
-              <FontAwesomeIcon icon={faMoneyBill1} />
-              {` $ ${tour.price}`}
+              <FontAwesomeIcon icon={faDollarSign} />
+              {` ${tour.price}`}
             </span>
-          </div>
-          <p>{tour.information}</p>
+          </ContentIcons>
         </CardInfoContent>
       </TourCardInfo>
       <TourCardButtons className="box__buttons">
         {/* TODO: podria enviar solo el slug (siempre y cuando sea unico) */}
-        <Link to={`/tours-peru/${id}/${slug}`} className="btn-primary">
+        <Link to={`/tours-peru/${id}/${slug}`} className="btn-primary" style={borderWhite}>
           Saber más del tour
         </Link>
       </TourCardButtons>
