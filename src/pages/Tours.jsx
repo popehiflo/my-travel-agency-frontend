03466b74c05d5128a1010b64ff816fb690ca5413
popehiflo <poolpetterhijuel@hotmail.com>
2022-05-17 18:12:08 -0500

    feat: Temporal component for the show tours grid

--- src/pages/Tours.jsx
+++ src/pages/Tours.jsx
@@ -1,10 +1,16 @@
 import React from 'react';
+import { getAllTours } from '../dataFake';
 import ToursGrid from '../components/ToursGrid';
 
-const Tours = () => (
-  <section id="tours">
-    <ToursGrid />
-  </section>
-);
+// TODO: Add tours filter by category, pagination and sorting
+// FIXME: Temporal component to show the tours grid
+const Tours = () => {
+  const tours = getAllTours();
+  return (
+    <section id="tours">
+      <ToursGrid tours={tours} />
+    </section>
+  );
+};
 
 export default Tours;
