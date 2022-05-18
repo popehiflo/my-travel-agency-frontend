a05eb841ce4476bbec129a6b53b06a4e0f4f2a98
popehiflo <poolpetterhijuel@hotmail.com>
2022-05-18 09:16:43 -0500

    feat: TourTypes component, created

---
+++ src/components/TourTypes.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const TourTypes = () => (
+  <section>Tour Types</section>
+);
+
+export default TourTypes;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,12 +3,14 @@ import Banner from '../components/Banner';
 import Tours from './Tours';
 
 import { getToursByBanner } from '../dataFake';
+import TourTypes from '../components/TourTypes';
 
 const Home = () => {
   const toursBanner = getToursByBanner();
   return (
     <>
       <Banner data={toursBanner} idHash="home" content />
+      <TourTypes />
       <Tours />
     </>
   );
