398253e618f2785e12427fd3199116afdb3919aa
popehiflo <poolpetterhijuel@hotmail.com>
2022-05-22 09:27:46 -0500

    feat: Tours component, list of tours for filter

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,11 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import Home from './pages/Home';
+import Backpack from './pages/Backpack';
 import SignIn from './pages/SignIn';
 import SignUp from './pages/SignUp';
 import Tour from './pages/Tour';
-import Backpack from './pages/Backpack';
+import Tours from './pages/Tours';
 
 const App = () => (
   <BrowserRouter>
@@ -20,6 +21,7 @@ const App = () => (
         <Route path="nested-b" element={<h1 style={{ marginTop: '100px' }}>Nested BBB</h1>} />
       </Route>
       <Route path="/backpack" element={<Backpack />} />
+      <Route path="tours" element={<Tours />} />
       <Route path="tours-peru/:slug" element={<Tour />} />
       <Route path="/admin-panel" element={<h1 style={{ marginTop: '100px' }}>Admin Panel</h1>} />
       <Route path="*" element={<h1 style={{ marginTop: '100px' }}>404</h1>} />
