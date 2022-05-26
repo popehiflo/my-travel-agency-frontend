51e3eab62f448d46eabaa864f18cb2de0e080d6b
popehiflo <poolpetterhijuel@hotmail.com>
2022-05-26 09:40:39 -0500

    feat: ajustes

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Route, Routes, Navigate } from 'react-router-dom';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import Home from './pages/Home';
@@ -8,27 +8,30 @@ import SignUp from './pages/SignUp';
 import Tour from './pages/Tour';
 import Tours from './pages/Tours';
 
-const App = () => (
-  <BrowserRouter>
-    <Header />
+const App = () => {
+  const userLogged = true;
+  return (
+    <BrowserRouter>
+      <Header />
 
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/account/sign-in" element={<SignIn />} />
-      <Route path="/account/sign-up" element={<SignUp />} />
-      <Route path="/test-nested" element={<h1 style={{ marginTop: '100px' }}>Test Nested</h1>}>
-        <Route path="nested-a" element={<h1 style={{ marginTop: '100px' }}>Nested AAA</h1>} />
-        <Route path="nested-b" element={<h1 style={{ marginTop: '100px' }}>Nested BBB</h1>} />
-      </Route>
-      <Route path="/backpack" element={<Backpack />} />
-      <Route path="tours" element={<Tours />} />
-      <Route path="tours-peru/:slug" element={<Tour />} />
-      <Route path="/admin-panel" element={<h1 style={{ marginTop: '100px' }}>Admin Panel</h1>} />
-      <Route path="*" element={<h1 style={{ marginTop: '100px' }}>404</h1>} />
-    </Routes>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/account/sign-in" element={userLogged ? <Navigate to="/" /> : <SignIn />} />
+        <Route path="/account/sign-up" element={userLogged ? <Navigate to="/" /> : <SignUp />} />
+        <Route path="/backpack" element={<Backpack />} />
+        <Route path="/tours/:category" element={<Tours />} />
+        <Route path="/tour/:slug" element={<Tour />} />
+        <Route path="/admin-panel" element={<h1 style={{ marginTop: '100px' }}>Admin Panel</h1>} />
+        <Route path="/test-nested" element={<h1 style={{ marginTop: '100px' }}>Test Nested</h1>}>
+          <Route path="nested-a" element={<h1 style={{ marginTop: '100px' }}>Nested AAA</h1>} />
+          <Route path="nested-b" element={<h1 style={{ marginTop: '100px' }}>Nested BBB</h1>} />
+        </Route>
+        <Route path="*" element={<h1 style={{ marginTop: '100px' }}>404</h1>} />
+      </Routes>
 
-    <Footer />
-  </BrowserRouter>
-);
+      <Footer />
+    </BrowserRouter>
+  );
+};
 
 export default App;
