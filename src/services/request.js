175da107b398fa9f919056d18b7cbfe2f7da5de3
popehiflo <poolpetterhijuel@hotmail.com>
2022-05-26 18:16:46 -0500

    feat: Aqui ira la orden de ejecucion del tour

--- src/services/request.js
+++ src/services/request.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const BASE_URL = `${process.env.REACT_APP_API_URL}`;
-const TOKEN = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYyOGQzOGJkNjA5MjM3OTQ4ZjcwZDllZiIsImlzQWRtaW4iOmZhbHNlLCJyb2xlIjoiQURNSU4iLCJpYXQiOjE2NTM0MjI2NTgsImV4cCI6MTY1MzY4MTg1OH0.7Briur62nkRGprdFrOtHJT98mAXrMD1nT7YZ_zUj0gU';
+const TOKEN = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYyOGZlMWE5MWQxYjIxZTQ4MDAwZTg1YiIsImlzQWRtaW4iOnRydWUsInJvbGUiOiJBRE1JTiIsImlhdCI6MTY1MzU5NjcxNywiZXhwIjoxNjUzODU1OTE3fQ.j3ZmyzW_0ZBPb-sIxQjFYVSbtnHbBXsUGkT2LaicD14';
 
 export const publicRequest = axios.create({
   baseURL: BASE_URL,
@@ -9,5 +9,5 @@ export const publicRequest = axios.create({
 
 export const privateRequest = axios.create({
   baseURL: BASE_URL,
-  headers: { 'x-token': `Bearer ${TOKEN}` },
+  header: { 'x-token': `Bearer ${TOKEN}` },
 });
